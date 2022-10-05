a2c9eaef14349bf8d71f4bb47a0149cf1043714c
Arijit1000 <73710898+Arijit1000@users.noreply.github.com>
2022-10-05 23:24:04 +0530

    Update starter.py
    Updated the code snippet for Insert Interval

--- coding/insert_interval/starter.py
+++ coding/insert_interval/starter.py
@@ -2,5 +2,22 @@ class Solution:
     # @params:
     # - intervals: List[List[int]]
     # - newInterview: List[int]
-    def insert_interval(self, intervals, newInterval):
-        pass
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        new_start, new_end = newInterval
+        result = []
+        for i, (start, end) in enumerate(intervals):
+            if end < new_start:
+                result.append([start, end])
+            elif end <= new_end:
+                new_start = min(start, new_start)
+            elif start <= new_end:
+                new_start = min(start, new_start)
+                new_end = end
+            elif start > new_end:
+                result.append([new_start, new_end])
+                result += intervals[i:]
+                break
+        else:
+            result.append([new_start, new_end])
+            
+        return result
